e1b7c25efb76fdbcff3e1abc0d29455a572778e7
aleefiya nusrah <muhaallameen@gmail.com>
2025-07-06 16:52:16 +0530

    add 3000 port to cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ export const connection = connectToDatabase()
 
 const app = express()
 app.use(cors({
-    origin: 'http://localhost:5173', // Allow requests from this origin
+    origin: ['http://localhost:5173', 'http://localhost:3000'], // Allow requests from these origins
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed methods
     credentials: true // Allow credentials (if needed)
 }));
